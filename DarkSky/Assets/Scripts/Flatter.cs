4dd52f6bc2b516343f33add10a52f7e21d1b89e6
CloneWorks <cloneworks24@gmail.com>
2017-01-30 14:19:59 +1100

    Added Y coord to flatter
    :)

--- DarkSky/Assets/Scripts/Flatter.cs
+++ DarkSky/Assets/Scripts/Flatter.cs
@@ -8,6 +8,7 @@ public class Flatter : MonoBehaviour
 
     float[,] heightmapData; // prepare a matrix with terrain points
     float ratio; // ratio between player position and terrain points
+    float ratioY; 
 
     // Use this for initialization
     void Start()
@@ -20,6 +21,7 @@ public class Flatter : MonoBehaviour
         heightmapData = terrData.GetHeights(0, 0, terrRes, terrRes); // heights we will change during of walking
 
         ratio = (terrSize.x) / terrRes;
+        
 
         Debug.Log("heightmapData=" + heightmapData + " terrRes=" + terrRes + " terrSize=" + terrSize + " ratio=" + ratio);
     }
@@ -30,22 +32,29 @@ public class Flatter : MonoBehaviour
         //update terrain
         terrData = Terrain.activeTerrain.terrainData;
         int terrRes = terrData.heightmapResolution;
+        int terrResY = terrData.heightmapHeight;
+
         Vector3 terrSize = terrData.size;
 
         heightmapData = terrData.GetHeights(0, 0, terrRes, terrRes); // heights we will change during of walking
 
-        ratio = (terrSize.x) / terrRes;
+        ratio = (terrSize.x) / terrResY;
+        ratioY = (terrSize.y);
 
         //work out players position (player pos + terrain offset)
         float playPosX = transform.position.x + (terrData.size.x / 2);
         float playPosZ = transform.position.z + (terrData.size.z / 2);
 
+        float playPosY = transform.position.y + (terrData.size.y);
+
         int terrainPointZ = Mathf.CeilToInt(playPosX / ratio);
         int terrainPointX = Mathf.CeilToInt(playPosZ / ratio);
 
-        Debug.Log(" terrainPointX=" + terrainPointX + " x terrainPointZ=" + terrainPointZ + ": set height to 0.0");
+        int terrainPointY = Mathf.CeilToInt(playPosY / ratioY);
 
-        heightmapData[terrainPointX, terrainPointZ] = 0.0f; // move terrain point to 0 (example)
+        Debug.Log(" terrainPointX=" + terrainPointX + " x terrainPointZ=" + terrainPointZ + ": set height to: " + terrainPointY);
+
+        heightmapData[terrainPointX, terrainPointZ] = terrainPointY; // move terrain point to 0 (example)
 
     }
 
