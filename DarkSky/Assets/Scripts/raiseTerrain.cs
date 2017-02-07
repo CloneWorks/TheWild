517a0f8a9bb3877831776d8380edc10995e3bd83
CloneWorks <cloneworks24@gmail.com>
2017-02-07 18:33:18 +1100

    TerrainRaiser
    Basically I'm a god ;)

---
+++ DarkSky/Assets/Scripts/raiseTerrain.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raiseTerrain : MonoBehaviour {
+
+    public Terrain terrain;
+
+    public float heightChange;
+
+	// Use this for initialization
+	void Start () {
+        increaseTerrainHeight(heightChange);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void increaseTerrainHeight(float height)
+    {
+        float[,] current = terrain.terrainData.GetHeights(0, 0, terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight);
+        float[,] hts = new float[terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight];
+        for (int i = 0; i < terrain.terrainData.heightmapWidth; i++)
+        {
+            for (int k = 0; k < terrain.terrainData.heightmapHeight; k++)
+            {
+                hts[i, k] = height + current[i, k];
+            }
+        }
+
+        terrain.terrainData.SetHeights(0, 0, hts);
+    }
+}
