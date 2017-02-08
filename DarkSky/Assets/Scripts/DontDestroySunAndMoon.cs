efe805ace5165833fb5c1cb18341d38f8388d63a
CloneWorks <cloneworks24@gmail.com>
2017-02-08 17:53:51 +1100

    sun and moon turn off in dungeons
    YAYA

--- DarkSky/Assets/Scripts/DontDestroySunAndMoon.cs
+++ DarkSky/Assets/Scripts/DontDestroySunAndMoon.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroySunAndMoon : MonoBehaviour {
 
@@ -23,5 +24,28 @@ public class DontDestroySunAndMoon : MonoBehaviour {
 
         //Sets this to not be destroyed when reloading scene
         DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        //when in a dungeon
+        if(SceneManager.GetActiveScene().buildIndex == 3)
+        {
+            GetComponentInChildren<Light>().enabled = false;
+
+            foreach(Renderer r in gameObject.GetComponentsInChildren<Renderer>())
+            {
+                r.enabled = false;
+            }
+        }
+        else
+        {
+            GetComponentInChildren<Light>().enabled = true;
+
+            foreach (Renderer r in gameObject.GetComponentsInChildren<Renderer>())
+            {
+                r.enabled = true;
+            }
+        }
     }
 }
