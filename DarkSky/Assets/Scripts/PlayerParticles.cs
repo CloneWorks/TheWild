3fa7e3b171f2bfbd05f59eeea3cb1a4ac535b717
CloneWorks <cloneworks24@gmail.com>
2017-02-11 22:08:52 +1100

    PlayerWaterRipples
    In Summary

---
+++ DarkSky/Assets/Scripts/PlayerParticles.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerParticles : MonoBehaviour {
+
+    public ParticleSystem psRipple;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //entering
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Water")
+        {
+            //move ripple up and down player to the water level
+            psRipple.transform.position = new Vector3(psRipple.transform.position.x, other.transform.position.y, psRipple.transform.position.z);
+
+            //turn on
+            psRipple.Play();
+        }
+    }
+
+    //leaving
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Water")
+        {
+            //turn off
+            psRipple.Stop();
+        }
+    }
+}
