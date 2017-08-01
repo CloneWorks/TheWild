19fa9688f4d6f139ab3c5babb330fc36d0d221cc
CloneWorks <cloneworks24@gmail.com>
2017-08-02 07:38:42 +1000

    Started working on a cut scene and fixed woriks square hills in the town.

---
+++ DarkSky/Assets/Scripts/Animation/AnimationCameraSwitch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCameraSwitch : MonoBehaviour {
+
+	public Camera main;
+	public Camera animation; 
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+		PlayerEnabled (false);
+		main.enabled = false;
+		animation.enabled = true;
+	}
+
+	void PlayerEnabled(bool enabled)
+	{
+		//for (int i = 0; i < player.transform.childCount; i++)
+		//{
+		//	player.transform.GetChild (i).gameObject.SetActive(false);
+		//}
+		MyWait();
+		player.GetComponent<BasicBehaviour> ().enabled = enabled;
+		player.GetComponent<PlayerWeapon> ().enabled = enabled;
+		main.GetComponent<ThirdPersonOrbitCam> ().enabled = enabled;
+	}
+
+	IEnumerator MyWait()
+	{
+		yield return new WaitForSeconds (20);
+	}
+}
